7437b7e21316f21b8946837f2f356f700a9287c2
implodingduck <zsek320@gmail.com>
2021-07-19 09:37:59 -0500

    adding sql

--- main.tf
+++ main.tf
@@ -37,3 +37,63 @@ resource "random_string" "unique" {
   special = false
   upper   = false
 }
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "apim-logicapp-kv"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+      "purge",
+      "recover",
+      "delete"
+    ]
+
+    secret_permissions = [
+      "set",
+      "purge",
+      "get",
+      "list"
+    ]
+
+    certificate_permissions = [
+      "purge"
+    ]
+
+    storage_permissions = [
+      "purge"
+    ]
+  }
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "azurerm_key_vault_secret" "dbpassword" {
+  name         = "dbpassword"
+  value        = random_password.password.result
+  key_vault_id = azurerm_key_vault.kv.id
+  tags         = {}
+}
+
+module "sql" {
+  source = "github.com/implodingduck/tfmodules//sql"
+  name = local.func_name
+  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_location = azurerm_resource_group.rg.location
+  db_password = random_password.password.result
+}
+
