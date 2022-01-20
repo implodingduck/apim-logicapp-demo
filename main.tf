5c4ce1f45900a25901bdc840b34443472eb2952f
implodingduck <zsek320@gmail.com>
2022-01-20 06:18:05 -0600

    again

--- main.tf
+++ main.tf
@@ -274,12 +274,12 @@ resource "azurerm_private_dns_zone" "functions" {
   resource_group_name       = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "functions" {
-  name                  = "functions"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.functions.name
-  virtual_network_id    = azurerm_virtual_network.default.id
-}
+# resource "azurerm_private_dns_zone_virtual_network_link" "functions" {
+#   name                  = "functions"
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   private_dns_zone_name = azurerm_private_dns_zone.functions.name
+#   virtual_network_id    = azurerm_virtual_network.default.id
+# }
 
 resource "azurerm_private_endpoint" "pe" {
   name                = "pe-sa${local.func_name}"
@@ -299,23 +299,23 @@ resource "azurerm_private_endpoint" "pe" {
   }
 }
 
-resource "azurerm_private_endpoint" "logicapp" {
-  name                = "pe-la${local.func_name}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.pe.id
+# resource "azurerm_private_endpoint" "logicapp" {
+#   name                = "pe-la${local.func_name}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = azurerm_subnet.pe.id
 
-  private_service_connection {
-    name                           = "pe-connection-la${local.func_name}"
-    private_connection_resource_id = azurerm_logic_app_standard.example.id
-    is_manual_connection           = false
-    subresource_names              = ["sites"]
-  }
-  private_dns_zone_group {
-    name                 = azurerm_private_dns_zone.functions.name
-    private_dns_zone_ids = [azurerm_private_dns_zone.functions.id]
-  }
-}
+#   private_service_connection {
+#     name                           = "pe-connection-la${local.func_name}"
+#     private_connection_resource_id = azurerm_logic_app_standard.example.id
+#     is_manual_connection           = false
+#     subresource_names              = ["sites"]
+#   }
+#   private_dns_zone_group {
+#     name                 = azurerm_private_dns_zone.functions.name
+#     private_dns_zone_ids = [azurerm_private_dns_zone.functions.id]
+#   }
+# }
 
 
 resource "azurerm_storage_account" "sa" {
@@ -363,47 +363,47 @@ resource "azurerm_app_service_plan" "asp" {
   tags = local.tags
 }
 
-resource "azurerm_logic_app_standard" "example" {
-  name                       = "la-${local.func_name}"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.asp.id
-  storage_account_name       = azurerm_storage_account.sa.name
-  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME"       = "node"
-    "WEBSITE_NODE_DEFAULT_VERSION"   = "~12"
-    "WEBSITE_CONTENTOVERVNET"        = "1"
-    "WEBSITE_VNET_ROUTE_ALL"         = "1"
-    "sql_connectionString"           = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv.name};SecretName=${azurerm_key_vault_secret.dbconnectionstring.name})"
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app.instrumentation_key
-  }
-  identity {
-    type = "SystemAssigned"
-  }
-  tags = local.tags
-}
+# resource "azurerm_logic_app_standard" "example" {
+#   name                       = "la-${local.func_name}"
+#   location                   = azurerm_resource_group.rg.location
+#   resource_group_name        = azurerm_resource_group.rg.name
+#   app_service_plan_id        = azurerm_app_service_plan.asp.id
+#   storage_account_name       = azurerm_storage_account.sa.name
+#   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+#   app_settings = {
+#     "FUNCTIONS_WORKER_RUNTIME"       = "node"
+#     "WEBSITE_NODE_DEFAULT_VERSION"   = "~12"
+#     "WEBSITE_CONTENTOVERVNET"        = "1"
+#     "WEBSITE_VNET_ROUTE_ALL"         = "1"
+#     "sql_connectionString"           = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv.name};SecretName=${azurerm_key_vault_secret.dbconnectionstring.name})"
+#     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app.instrumentation_key
+#   }
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#   tags = local.tags
+# }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "example" {
-  app_service_id = azurerm_logic_app_standard.example.id
-  subnet_id      = azurerm_subnet.logicapps.id
-}
+# resource "azurerm_app_service_virtual_network_swift_connection" "example" {
+#   app_service_id = azurerm_logic_app_standard.example.id
+#   subnet_id      = azurerm_subnet.logicapps.id
+# }
 
-resource "azurerm_key_vault_access_policy" "la" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = azurerm_logic_app_standard.example.identity.0.principal_id
-  secret_permissions = [
-    "get",
-    "list"
-  ]
-}
+# resource "azurerm_key_vault_access_policy" "la" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id = data.azurerm_client_config.current.tenant_id
+#   object_id = azurerm_logic_app_standard.example.identity.0.principal_id
+#   secret_permissions = [
+#     "get",
+#     "list"
+#   ]
+# }
 
-resource "azurerm_role_assignment" "sa" {
-  scope                = azurerm_storage_account.sa.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_logic_app_standard.example.identity.0.principal_id
-}
+# resource "azurerm_role_assignment" "sa" {
+#   scope                = azurerm_storage_account.sa.id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = azurerm_logic_app_standard.example.identity.0.principal_id
+# }
 resource "azurerm_key_vault_access_policy" "client-config" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id = data.azurerm_client_config.current.tenant_id
