7e27bdd374b606cf38f6c96187c597005510f359
implodingduck <zsek320@gmail.com>
2022-01-21 10:22:47 -0600

    using new asp

--- main.tf
+++ main.tf
@@ -534,7 +534,7 @@ resource "azurerm_function_app" "func" {
   name                       = "${local.func_name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  app_service_plan_id        = azurerm_app_service_plan.aspfunc.id
   storage_account_name       = azurerm_storage_account.safunc.name
   storage_account_access_key = azurerm_storage_account.safunc.primary_access_key
   version = "~4"
