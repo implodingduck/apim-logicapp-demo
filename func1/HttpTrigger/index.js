aa75dc9ea99874d7f24680e37ee701fcf7529881
implodingduck <zsek320@gmail.com>
2022-01-21 10:41:43 -0600

    changing to a post

--- func1/HttpTrigger/index.js
+++ func1/HttpTrigger/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const laurl = process.env.LA_URL
     context.log(`preaxios:${laurl}`)
-    const res = await axios.get(`${laurl}`);
+    const res = await axios.post(`${laurl}`);
     context.log(res.data)
     context.log("postaxios")
     context.res = {
